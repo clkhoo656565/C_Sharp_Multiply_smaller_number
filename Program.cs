9653562a48c4efbf2d61513250dfa8b1b26300bd
CHEE LONG KHOO <khoo@dxc.com>
2022-03-04 17:41:56 +0800

    Multiply smaller numbers of the input

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Multiply_smaller_number
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a integer:");
+            string input = Console.ReadLine();
+
+            int result = 0;
+
+            if (input.Length > 0)
+            {
+                int number = Convert.ToInt32(input);
+
+                for (int i = 1; i < number; i++)
+                {
+                    if (result == 0)
+                    {
+                        result = number * i;
+                    }
+                    else
+                    {
+                        result = result * i;
+                    }
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # C_Sharp_Multiply_smaller_number
 Multiply all the smaller number(s) than the input number
+
+### The calculator can handle the scenarios below:
+- If user input 6, then the program will count 6 * 5 * 4 * 3 * 2 * 1
+- If user input 10, then the program will count  10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
